85e92a7289c9576096f4379dbce8d11d4676321a
PlusTM <telekingzarin@gmail.com>
2016-12-24 04:57:40 -0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,39 @@
+
+
+token = 'Token' # Token
+civey = '297069483' # Id
+about = 'Your About Fa Text' #Your About in Persian
+abouten = 'Your About en Text' #English About
+creatorfa = 'Creator Fa About' #Cereator persian About
+creatoren = 'Creator En About' #Creator english About
+contact = 'Your Phone Number' #Examp = +98 939 570 7155
+contactname = 'Your Contact Name' # Your Name For Contact Name
+channel = 'https://telegram.me/SKYTMS' #Your Channel Id
+startfa = 'Your fa Start Massage For Members' #persian Start massage Set
+starten = 'Your en Start Massage For Members' #english Start massage Set
+photo = 'bot Massage is Member send photo' #bot massaage to Mmeber For send Photo
+text = 'bot Massage is Member send text' #bot massaage to Mmeber For send text
+document = 'bot Massage is Member send file' #bot massaage to Mmeber For send file
+sticker = 'bot Massage is Member send sticker' #bot massaage to Mmeber For send sticker
+audio = 'bot Massage is Member send audio' #bot massaage to Mmeber For send audio
+contact = 'bot Massage is Member send contact' #bot massaage to Mmeber For send contact
+bantime = 'Bot massage for member banned' #bot send massage to member banned
+unbantime = 'Bot massage for member unbanned' #bot send massage to member unbanned
+
+
+#                  _____
+#                 |_____|
+#  ______________ ______  ____         ____  _________    ____       ____
+# |              /      /|    |       |    |/         \  |    |     |    |
+# |    __________/      /|    |       |    |     __    | |    |     |    |
+# |   |          /      /|    |       |    |    |__|   | |    |_____|    |
+# |   |          /      /|    |       |    |    ______ / \               |
+# |   |__________/      /|     _______     |   |_______   \             /
+# |              /      /|                 |           |   \________   /
+# |______________/______/|_________________|\__________|    ___       /  
+#                                                          |         /
+#                                                          |_______ /
+#
+#Copy right  2016 Yones Civey - SKY TM / PlusTM
+
+#@SKYTMS
